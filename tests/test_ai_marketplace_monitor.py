47d4897bc6a55d69897543814d72b9a835df1615
Bo Peng <ben.bob@gmail.com>
2025-03-09 11:19:31 -0500

    Release version 0.8.6

--- tests/test_ai_marketplace_monitor.py
+++ tests/test_ai_marketplace_monitor.py
@@ -13,7 +13,7 @@ from ai_marketplace_monitor.user import User
 
 def test_version(version: str) -> None:
     """Sample pytest test function with the pytest fixture as an argument."""
-    assert version == "0.8.6b1"
+    assert version == "0.8.6"
 
 
 def test_listing_cache(temp_cache: Cache, listing: Listing) -> None:
