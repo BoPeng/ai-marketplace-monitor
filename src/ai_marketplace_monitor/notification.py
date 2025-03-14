bc2df5d162c93600bbce256910d0c5690ecf417d
Bo Peng <ben.bob@gmail.com>
2025-03-13 19:43:18 -0500

    Refactorize push notification logic

--- src/ai_marketplace_monitor/notification.py
+++ src/ai_marketplace_monitor/notification.py
@@ -1,8 +1,15 @@
+import time
+from collections import defaultdict
 from dataclasses import dataclass, fields
 from enum import Enum
-from typing import Any, Optional, Type
+from logging import Logger
+from typing import Any, ClassVar, DefaultDict, List, Optional, Tuple, Type
 
-from .utils import BaseConfig
+import inflect
+
+from .ai import AIResponse  # type: ignore
+from .listing import Listing
+from .utils import BaseConfig, hilight
 
 
 class NotificationStatus(Enum):
@@ -42,3 +49,118 @@ class NotificationConfig(BaseConfig):
             # subclases
             succ.append(subclass.notify_all(config, *args, **kwargs))
         return any(succ)
+
+
+@dataclass
+class PushNotificationConfig(NotificationConfig):
+    required_fields: ClassVar[List[str]] = []
+
+    def notify(
+        self: "PushNotificationConfig",
+        listings: List[Listing],
+        ratings: List[AIResponse],
+        notification_status: List[NotificationStatus],
+        force: bool = False,
+        logger: Logger | None = None,
+    ) -> bool:
+        for field in self.required_fields:
+            if not getattr(self, field, None):
+                if logger:
+                    logger.debug(f"No {hilight(field)} specified.")
+                return False
+
+        #
+        # we send listings with different status with different messages
+        msgs: DefaultDict[NotificationStatus, List[Tuple[Listing, str]]] = defaultdict(list)
+        p = inflect.engine()
+        for listing, rating, ns in zip(listings, ratings, notification_status):
+            if ns == NotificationStatus.NOTIFIED and not force:
+                continue
+            msg = (
+                (
+                    f"{listing.title}\n{listing.price}, {listing.location}\n"
+                    f"{listing.post_url.split('?')[0]}"
+                )
+                if rating.comment == AIResponse.NOT_EVALUATED
+                else (
+                    f"[{rating.conclusion} ({rating.score})] {listing.title}\n"
+                    f"{listing.price}, {listing.location}\n"
+                    f"{listing.post_url.split('?')[0]}\n"
+                    f"AI: {rating.comment}"
+                )
+            )
+            msgs[ns].append((listing, msg))
+
+        if not msgs:
+            if logger:
+                logger.debug("No new listings to notify.")
+            return False
+
+        for ns, listing_msg in msgs.items():
+            if ns == NotificationStatus.NOT_NOTIFIED:
+                title = f"Found {len(listing_msg)} new {p.plural_noun(listing.name, len(listing_msg))} from {listing.marketplace}"
+            elif ns == NotificationStatus.EXPIRED:
+                title = f"Another look at {len(listing_msg)} {p.plural_noun(listing.name, len(listing_msg))} from {listing.marketplace}"
+            elif ns == NotificationStatus.LISTING_CHANGED:
+                title = f"Found {len(listing_msg)} updated {p.plural_noun(listing.name, len(listing_msg))} from {listing.marketplace}"
+            elif ns == NotificationStatus.LISTING_DISCOUNTED:
+                title = f"Found {len(listing_msg)} discounted {p.plural_noun(listing.name, len(listing_msg))} from {listing.marketplace}"
+            else:
+                title = f"Resend {len(listing_msg)} {p.plural_noun(listing.name, len(listing_msg))} from {listing.marketplace}"
+
+            message = "\n\n".join([x[1] for x in listing_msg])
+            #
+            if not self.send_push_message_with_retry(title, message, logger=logger):
+                return False
+        return True
+
+    def send_push_message_with_retry(
+        self: "PushNotificationConfig",
+        title: str,
+        message: str,
+        max_retries: int = 6,
+        delay: int = 10,
+        logger: Logger | None = None,
+    ) -> bool:
+        for field in self.required_fields:
+            if not getattr(self, field, None):
+                if logger:
+                    logger.debug(f"No {hilight(field)} specified.")
+                return False
+
+        for attempt in range(max_retries):
+            try:
+                res = self.send_push_message(title=title, message=message, logger=logger)
+                if logger:
+                    logger.info(
+                        f"""{hilight("[Notify]", "succ")} Sent {self.name} a message with title {hilight(title)}"""
+                    )
+                return res
+            except KeyboardInterrupt:
+                raise
+            except Exception as e:
+                if logger:
+                    logger.debug(
+                        f"""{hilight("[Notify]", "fail")} Attempt {attempt + 1} failed: {e}"""
+                    )
+                if attempt < max_retries - 1:
+                    if logger:
+                        logger.debug(
+                            f"""{hilight("[Notify]", "fail")} Retrying in {delay} seconds..."""
+                        )
+                    time.sleep(delay)
+                else:
+                    if logger:
+                        logger.error(
+                            f"""{hilight("[Notify]", "fail")} Max retries reached. Failed to push note to {self.name}."""
+                        )
+                    return False
+        return False
+
+    def send_push_message(
+        self: "PushNotificationConfig",
+        title: str,
+        message: str,
+        logger: Logger | None = None,
+    ) -> bool:
+        raise NotImplementedError("send_push_message needs to be defined.")
